d9c4cc32f9b682e41c3250892cbec799725100f1
VasilikiPapageorgiou <vasil.papage@gmail.com>
2020-08-19 01:14:38 +0300

    changed background color

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Homepage() {
 
     return (
-        <>
+        <div className="homepage-try">
             <h1 className="homepage-title"> Air Pollution in Seoul </h1>
             <h3 className="homepage-subtitle">
                 This web application deals with air pollution measurement information in Seoul, South Korea. You can choose one of the stations (101 to 125) <br /> where the measurements are made and a chemical compound.
@@ -14,7 +14,7 @@ function Homepage() {
             <Link to="/search" className="homepage-button">
                 Search for Pollution Data
             </Link>
-        </>
+        </div>
     )
 
 }
