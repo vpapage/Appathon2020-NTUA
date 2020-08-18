9c0cbdcd07199fce32f37d1579e11833d35a6f94
VasilikiPapageorgiou <vasil.papage@gmail.com>
2020-08-18 13:28:51 +0300

    fix state in search results component

--- frontend/src/components/search-results.js
+++ frontend/src/components/search-results.js
@@ -5,68 +5,64 @@ const SearchResults = (props) => {
 
     const [results, setResults] = useState();
     const [compound, setConpound] = useState();
-    const [chartData, setChartData] = useState();
 
     useEffect(() => {
         setResults(props.results);
         setConpound(props.compound);
-
-        if (props.results) {
-
-            console.log(compound);
-            const labels = props.results.map(el => el['MeasurementDate']);
-            
-            const datasets = [
-                {
-                    label: 'CO',
-                    data: props.results.map(el => el['CO']),
-                    borderColor: 'blue',
-                    fill: false,
-                    hidden: (compound !== 'CO')
-                }, {
-                    label: 'O3',
-                    data: props.results.map(el => el['O3']),
-                    borderColor: 'purple',
-                    fill: false,
-                    hidden: (compound !== 'O3')
-                }, {
-                    label: 'NO2',
-                    data: props.results.map(el => el['NO2']),
-                    borderColor: 'green',
-                    fill: false,
-                    hidden: (compound !== 'NO2')
-                }, 
-                {
-                    label: 'SO2',
-                    data: props.results.map(el => el['SO2']),
-                    borderColor: 'yellow',
-                    fill: false,
-                    hidden: (compound !== 'SO2')
-                }, {
-                    label: 'PM10',
-                    data: props.results.map(el => el['PM10']),
-                    borderColor: 'red',
-                    fill: false,
-                    hidden: (compound !== 'PM10')
-                }
-            ];
-           
-            setChartData({
-                labels: labels,
-                datasets: datasets
-            })
-        };
-
     }, [props.results]);
 
+    const chartData = rawData => {
+        
+        const labels = rawData.map(el => el['MeasurementDate']);
+        
+        const datasets = [
+            {
+                label: 'CO',
+                data: rawData.map(el => el['CO']),
+                borderColor: 'blue',
+                fill: false,
+                hidden: (compound !== 'CO')
+            }, {
+                label: 'O3',
+                data: rawData.map(el => el['O3']),
+                borderColor: 'purple',
+                fill: false,
+                hidden: (compound !== 'O3')
+            }, {
+                label: 'NO2',
+                data: rawData.map(el => el['NO2']),
+                borderColor: 'green',
+                fill: false,
+                hidden: (compound !== 'NO2')
+            }, 
+            {
+                label: 'SO2',
+                data: rawData.map(el => el['SO2']),
+                borderColor: 'yellow',
+                fill: false,
+                hidden: (compound !== 'SO2')
+            }, {
+                label: 'PM10',
+                data: rawData.map(el => el['PM10']),
+                borderColor: 'red',
+                fill: false,
+                hidden: (compound !== 'PM10')
+            }
+        ];
+       
+        return {
+            labels: labels,
+            datasets: datasets
+        };
+    };
 
     return (
         <>
-            {chartData && <Line
-                data={chartData}
+            {results && <Line
+                data={chartData(results)}
                 width={600}
                 height={250}
-                options={{ showLines: true }}
+                options={{ showLines: false }}
             />}
         </>
     );
